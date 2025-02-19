4b7b2ef0dfb99d0b5f5ed448301bfdf421866c98
JesseMambu12 <jessechristianmambu@gmail.com>
2025-02-19 15:04:46 +0700

    fixed todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,76 @@
-import Image from "next/image";
-
 export default function HeaderComponent() {
   return (
     <div>
-    {/* Header */}
-    <div className="relative w-full h-[35vh] overflow-hidden">
-      <img 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAAG02k9uomeIqLlL37tXK8blgKyF_8umwOw&s" 
-        alt="foto header" 
-        width={1920}
-        height={1080}
-        className="w-full h-full object-cover"
-      />
-
-      {/* Add New TODO */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center gap-6">
-        <h1 className="text-white text-4xl font-bold drop-shadow-lg tracking-widest">
-          TODO
-        </h1>
-        <input 
-          type="text" 
-          placeholder="Create a new todo..." 
-          className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md focus:outline-none"
+      {/* Header */}
+      <div className="relative w-full h-[35vh] overflow-hidden">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAAG02k9uomeIqLlL37tXK8blgKyF_8umwOw&s"
+          alt="foto header"
+          width={1920}
+          height={1080}
+          className="w-full h-full object-cover"
         />
+
+        {/* Add New TODO */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-6">
+          <h1 className="text-white text-4xl font-bold drop-shadow-lg tracking-widest">
+            TODO
+          </h1>
+          <input
+            type="text"
+            placeholder="Create a new todo..."
+            className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md focus:outline-none"
+          />
+        </div>
       </div>
-    </div>
 
-    {/* Container for Checkbox */}
-    <div className="bg-white w-full py-10">
+      {/* Container for Checkbox */}
+      <div className="bg-white w-full py-10">
         <div className="flex flex-col items-center gap-4">
-
           {/* List Todo */}
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo1"/>
-            <label htmlFor="todo1" className="text-gray-800">Jog around the park</label>
+            <input type="checkbox" id="todo1" />
+            <label htmlFor="todo1" className="text-gray-800">
+              Jog around the park
+            </label>
           </div>
-          
+
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo2"/>
-            <label htmlFor="todo2" className="text-gray-800">10 minutes meditation</label>
+            <input type="checkbox" id="todo2" />
+            <label htmlFor="todo2" className="text-gray-800">
+              10 minutes meditation
+            </label>
           </div>
-          
+
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo3"/>
-            <label htmlFor="todo3" className="text-gray-800">Read for 1 hour</label>
+            <input type="checkbox" id="todo3" />
+            <label htmlFor="todo3" className="text-gray-800">
+              Read for 1 hour
+            </label>
           </div>
-          
+
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo4"/>
-            <label htmlFor="todo4" className="text-gray-800">Pick up groceries</label>
+            <input type="checkbox" id="todo4" />
+            <label htmlFor="todo4" className="text-gray-800">
+              Pick up groceries
+            </label>
           </div>
-          
+
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo5"/>
-            <label htmlFor="todo5" className="text-gray-800">Complete TODO App on Fronted Mentor</label>
+            <input type="checkbox" id="todo5" />
+            <label htmlFor="todo5" className="text-gray-800">
+              Complete TODO App on Fronted Mentor
+            </label>
           </div>
           <div className="w-[60%] max-w-xl px-4 py-2 rounded-full shadow-md flex items-center gap-2">
-            <input type="checkbox" id="todo5"/>
-            <label htmlFor="todo5" className="text-gray-800">Complete TODO App on Fronted Mentor</label>
+            <input type="checkbox" id="todo5" />
+            <label htmlFor="todo5" className="text-gray-800">
+              Complete TODO App on Fronted Mentor
+            </label>
           </div>
         </div>
       </div>
-  </div>
+    </div>
   );
 }
 
